c94d839529a25628c67d2164820b72a0335d9dd5
Francisco Perez-Sorrosal <fperezsorrosal@gmail.com>
2019-08-18 11:02:41 -0700

    Add c4w2 material and solutions
    Some problems with feature_eng functions

---
+++ course4/Week 2/(OPTIONAL) A worked-out example for KD-trees _ Coursera_files/82.e3da14b52c0d1f6b1580.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[82],{"6JLt":function(module,exports,o){},qw8W:function(module,exports,o){var n=o("6JLt"),a;"string"==typeof n&&(n=[[module.i,n,""]]);var s={transform:void 0},t=o("aET+")(n,s);n.locals&&(module.exports=n.locals)}}]);
+//# sourceMappingURL=82.e3da14b52c0d1f6b1580.js.map
---
+++ course4/Week 2/(OPTIONAL) A worked-out example for KD-trees _ Coursera_files/allStyles.66368dfb4cc5ba62cf5b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["allStyles"],[]]);
+//# sourceMappingURL=allStyles.66368dfb4cc5ba62cf5b.js.map
